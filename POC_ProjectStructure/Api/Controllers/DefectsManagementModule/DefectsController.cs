97b7294e91f99eb4d8be4fc716abb9f950721046
Mak Thevar <mak.thevar11@gmail.com>
2023-04-12 11:48:06 +0530

    Add project files.

---
+++ POC_ProjectStructure/Api/Controllers/DefectsManagementModule/DefectsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using POC_ProjectStructure.BLL;
+
+namespace POC_ProjectStructure.Api.Controllers.DefectsManagementModule
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefectsController : ControllerBase
+    {
+        private readonly IDefectsService defectsService;
+
+        public DefectsController(IDefectsService defectsService)
+        {
+            this.defectsService = defectsService;
+        }
+        [HttpGet]
+        public async  Task<IActionResult> Get()
+        {
+            var response = await this.defectsService.GetMessage();
+            return Ok(response);
+        }
+    }
+}
---
+++ POC_ProjectStructure/BLL/DefectsService.cs
@@ -0,0 +1,16 @@
+﻿namespace POC_ProjectStructure.BLL
+{
+
+    public interface IDefectsService
+    {
+        Task<string> GetMessage();
+    }
+
+    public class DefectsService : IDefectsService
+    {
+        public Task<string> GetMessage()
+        {
+            return Task.FromResult("Defects controller works.");
+        }
+    }
+}
